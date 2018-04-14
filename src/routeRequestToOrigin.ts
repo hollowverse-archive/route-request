4643d10ddc4be96a018821dd2306e8675d184719
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-14 16:07:39 +0300

    Support paths on origin

---
+++ src/routeRequestToOrigin.ts
@@ -0,0 +1,87 @@
+import { CloudFrontRequestEvent } from 'aws-lambda'; // tslint:disable-line:no-implicit-dependencies
+import { createLambdaHandler } from '@hollowverse/utils/helpers/createLambdaHandler';
+import bluebird from 'bluebird';
+import find from 'lodash/find';
+import get from 'lodash/get';
+import { findEnvByName as findEbEnvByName } from './eb/findEnvByName';
+import { parse as parseUrl } from 'url';
+
+type CreateRouteRequestToOriginOptions = {
+  findEnvByName(branch: string): Promise<string | undefined>;
+};
+
+export const createRouteRequestToOrigin = ({
+  findEnvByName,
+}: CreateRouteRequestToOriginOptions) => async (
+  event: CloudFrontRequestEvent,
+) => {
+  const request = event.Records[0].cf.request;
+
+  const branch: string | undefined = get(request.headers, [
+    'x-hollowverse-requested-branch',
+    '0',
+    'value',
+  ]);
+
+  const env: string | undefined = get(request.headers, [
+    'x-hollowverse-assigned-environment',
+    '0',
+    'value',
+  ]);
+
+  const possibleEnvironments = await bluebird.map(
+    [branch, env],
+    async (envName, i) =>
+      envName
+        ? {
+            envName,
+            envUrl: await findEnvByName(envName),
+            isInternal: i === 0,
+          }
+        : undefined,
+  );
+  const resolvedEnvironment = find(possibleEnvironments, v => v !== undefined);
+
+  if (!resolvedEnvironment) {
+    throw new TypeError(
+      'Could not find any environment to route the request to',
+    );
+  }
+
+  const { envUrl } = resolvedEnvironment;
+
+  if (!envUrl) {
+    throw new TypeError(
+      `Could not find a URL for the requested environment ${env}`,
+    );
+  }
+
+  const { host: domainName, path = '' } = parseUrl(envUrl, undefined);
+
+  if (!domainName) {
+    throw new TypeError(`Expected host to be defined for URL: ${envUrl}`);
+  }
+
+  (request as any).origin = {
+    custom: {
+      domainName,
+      port: 80,
+      protocol: 'http',
+      sslProtocols: ['TLSv1', 'TLSv1.1'],
+      path,
+      readTimeout: 30,
+      keepaliveTimeout: 5,
+      customHeaders: {},
+    },
+  };
+
+  request.headers.host = [{ key: 'host', value: domainName }];
+
+  return request;
+};
+
+export const routeRequestToOrigin = createLambdaHandler(
+  createRouteRequestToOrigin({
+    findEnvByName: findEbEnvByName,
+  }),
+);
